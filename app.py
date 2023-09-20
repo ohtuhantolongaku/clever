7d80b36d8d697238a51139708fd97c63a3ed242c
ohtuhantolongaku <144606613+ohtuhantolongaku@users.noreply.github.com>
2023-09-20 16:37:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def run_commands():
     return jsonify({'output': output, 'errors': errors})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
